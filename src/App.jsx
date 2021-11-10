466ecc1717329484f2840b2db85fdbfb8f9d6be0
dhelarius <jeffrey.ayala2040@gmail.com>
2021-11-10 17:59:06 -0400

    3_install_reacttable

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,66 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import Table from './components/Table'
+import staticData from './config/staticData';
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const columns = React.useMemo(
+    () => [
+        {
+            Header: 'ID',
+            accessor: 'id'
+        },
+        {
+            Header: 'NPSV',
+            accessor: 'npsv'
+        },
+        {
+            Header: 'Name',
+            accessor: 'name'
+        },
+        {
+            Header: 'Lastname',
+            accessor: 'lastname'
+        },
+        {
+            Header: 'DNI',
+            accessor: 'dni'
+        },
+        {
+            Header: 'Phone',
+            accessor: 'phone'
+        },
+        {
+            Header: 'Email',
+            accessor: 'email'
+        },
+        {
+            Header: 'Creation date',
+            accessor: 'creationDate'
+        },
+        {
+            Header: 'Status',
+            accessor: 'status'
+        }
+    ],
+    []
+  );
+
+  const data = React.useMemo(() => staticData, []);
+
   return (
     <div className="App">
-      <div className="content">
+      <Table columns={columns} data={data} />
+      {/*<div className="content">
         <p className="text">React Template</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
-      </div>
+        </div>*/}
     </div>
   )
 }
---
+++ src/config/staticData.js
@@ -0,0 +1,26 @@
+const staticData = [
+    {
+        id: 1,
+        npsv: '1425',
+        name: 'Darío',
+        lastname: 'Jiménez',
+        dni: '402-2132140-5',
+        phone: '829-796-0691',
+        email: 'jeffrey.ayala2040@gmail.com',
+        creationDate: '10/11/2021',
+        status: true,
+    },
+    {
+        id: 2,
+        npsv: '0066',
+        name: 'Marleny',
+        lastname: 'Cepeda',
+        dni: '402-1454648-2',
+        phone: '829-499-0079',
+        email: 'marleon@gmail.com',
+        creationDate: '10/11/2021',
+        status: true,
+    }
+]
+
+export default staticData
