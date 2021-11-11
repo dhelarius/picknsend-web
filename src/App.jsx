a0280b579201fde8f715f410959905a4ce87fdc5
dhelarius <jeffrey.ayala2040@gmail.com>
2021-11-11 14:21:55 -0400

    1_add_table

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
+import React from 'react';
 import { useState } from 'react'
 import './App.css'
+import Table from './components/Table';
+import { staticData } from './config/staticValues'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Title",
+        accessor: "title",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+      {
+        Header: "Role",
+        accessor: "role",
+      },
+    ],
+    []
+  );
+
+  const data = React.useMemo(() => staticData(), [])
+
   return (
     <div className="App">
-      <div className="content">
-        <p className="text">React Template</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
+      <div>
+        <Table columns={columns} data={data} />
       </div>
     </div>
   )
